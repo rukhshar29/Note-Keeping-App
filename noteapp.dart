85f700f9d2c8b8215af67743d917a6f70382913a
rukhshar29 <156060229+rukhshar29@users.noreply.github.com>
2024-05-08 12:40:36 +0530

    Update noteapp.dart

--- noteapp.dart
+++ noteapp.dart
@@ -1,7 +1,4 @@
-//import 'dart:html';
-//import 'dart:ui';
 
-//import 'package:basic_crud_app/firebase_services.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:demo/services.dart';
 import 'package:flutter/material.dart';
@@ -27,7 +24,9 @@ class _HomePageeState extends State<HomePagee> {
     showDialog(context: context,
     builder: (context) {
       return AlertDialog( 
-        title: Text('Add your notes here',style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold,color: Color.fromARGB(255, 2, 68, 122),fontStyle: FontStyle.italic),),
+        title: Text('Add your notes here',style: TextStyle(
+          fontSize: 30,fontWeight: FontWeight.bold,color: Color.fromARGB(255, 2, 68, 122),
+          fontStyle: FontStyle.italic),),
         actions: [ElevatedButton(style: ElevatedButton.styleFrom(backgroundColor: Color.fromARGB(255, 3, 40, 69),
         
         ),
@@ -53,46 +52,9 @@ class _HomePageeState extends State<HomePagee> {
       );
     },);
   }
-  // TextEditingController controller = TextEditingController();
-  // FirestoreServices firestoreServices = FirestoreServices();
-  // void showNoteBox(String? textToedit, String? docId, Timestamp? time) {
-  //   showDialog(
-  //     context: context,
-  //     builder: (context) {
-  //       if (textToedit != null) {
-  //         controller.text = textToedit;
-  //       }
-  //       return AlertDialog(
-  //         title: Text(
-  //           "Add note",
-  //           style: GoogleFonts.alexandria(fontSize: 16),
-  //         ),
-  //         content: TextField(
-  //           decoration: InputDecoration(hintText: 'Note here...'),
-  //           style: GoogleFonts.alexandria(),
-  //           controller: controller,
-  //         ),
-  //         actions: [
-  //           ElevatedButton(
-  //             onPressed: () {
-  //               if (docId == null) {
-  //                 firestoreServices.addNote(controller.text);
-  //               } else {
-  //                 firestoreServices.updateNotes(docId, controller.text, time!);
-  //               }
-  //               controller.clear();
-  //               Navigator.pop(context);
-  //             },
-  //             child: Text(
-  //               'add',
-  //               style: GoogleFonts.alexandria(),
-  //             ),
-  //           )
-  //         ],
-  //       );
-  //     },
-  //   );
-  // }
+
+
+  
 
   @override
   Widget build(BuildContext context) {
@@ -193,106 +155,14 @@ class _HomePageeState extends State<HomePagee> {
           Center(child: Text('there is noting to show'),);
         }
       },),
-      // floatingActionButton: FloatingActionButton.extended(
-      //   backgroundColor: Colors.purple[100],
-      //   label: Text(
-      //     'add',
-      //     style: GoogleFonts.alexandria(fontSize: 18),
-      //   ),
-      //   icon: Icon(Icons.add),
-      //   onPressed: () async {
-      //     showNoteBox(null, null, null);
-      //   },
-      // ),
-      // body: StreamBuilder(
-      //   stream: FirestoreServices().showNotes(),
-      //   builder: (context, snapshot) {
-      //     if (snapshot.hasData) {
-      //       List noteList = snapshot.data!.docs;
-      //       return ListView.builder(
-      //         itemCount: noteList.length,
-      //         itemBuilder: (context, index) {
-      //           DocumentSnapshot document = noteList[index];
-      //           String docId = document.id;
-      //           Map<String, dynamic> data =
-      //               document.data() as Map<String, dynamic>;
-      //           String note = data['note'];
-      //           Timestamp time = data['timestamp'];
-      //           return Column(
-      //             children: [
-      //               Padding(
-      //                 padding: const EdgeInsets.symmetric(horizontal: 14),
-      //                 child: ListTile(
-      //                   contentPadding: EdgeInsets.all(16),
-      //                   shape: RoundedRectangleBorder(
-      //                       borderRadius: BorderRadius.circular(16)),
-      //                   tileColor: Colors.purple[100],
-      //                   title: Padding(
-      //                     padding: const EdgeInsets.all(8.0),
-      //                     child: Text(
-      //                       note,
-      //                       style: GoogleFonts.alexandria(
-      //                           textStyle: TextStyle(
-      //                               color: Colors.purple[800], fontSize: 19)),
-      //                     ),
-      //                   ),
-      //                   trailing: Column(
-      //                     children: [
-      //                       Row(
-      //                         mainAxisSize: MainAxisSize.min,
-      //                         children: [
-      //                           IconButton(
-      //                             color: Colors.purple[400],
-      //                             icon: Icon(Icons.edit),
-      //                             onPressed: () {
-      //                               showNoteBox(note, docId, time);
-      //                             },
-      //                           ),
-      //                           IconButton(
-      //                               color: Colors.purple[400],
-      //                               onPressed: () {
-      //                                 firestoreServices.deleteNote(docId);
-      //                               },
-      //                               icon: Icon(Icons.delete))
-      //                         ],
-      //                       ),
-      //                     ],
-      //                   ),
-      //                 ),
-      //               ),
-      //               Padding(
-      //                 padding: const EdgeInsets.symmetric(
-      //                     horizontal: 14, vertical: 4),
-      //                 child: Row(
-      //                   mainAxisAlignment: MainAxisAlignment.end,
-      //                   children: [
-      //                     Text(
-      //                       time.toDate().hour.toString(),
-      //                       style: const TextStyle(
-      //                           color: Colors.purple,
-      //                           fontWeight: FontWeight.bold),
-      //                     ),
-      //                     Text(":"),
-      //                     Text(
-      //                       time.toDate().minute.toString(),
-      //                       style: const TextStyle(
-      //                           color: Colors.purple,
-      //                           fontWeight: FontWeight.bold),
-      //                     ),
-      //                   ],
-      //                 ),
-      //               )
-      //             ],
-      //           );
-      //         },
-      //       );
-      //     } else {
-      //       return Center(
-      //         child: Text("Nohing to show...add notes"),
-      //       );
-      //     }
-      //   },
-      // ),
+, snapshot) {
+
+
+
+
+     
+
+
     );
   }
 }
